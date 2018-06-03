fb1d5fdcd38ad37b265329c185cf57dd3bd92c62
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2018-06-03 17:56:25 -0300

    advanced mappgin - crud OneToOne uni-directional

---
+++ hb-01-one-to-one-uni/src/com/feliphe/example/TestJdbc.java
@@ -0,0 +1,19 @@
+package com.feliphe.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&useTimezone=true&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		try {
+			Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Connection successful");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
